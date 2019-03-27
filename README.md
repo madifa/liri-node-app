45cf62aaa3f85eb915076125bd250b4757033357
madison fang <madisonfang0@gmail.com>
2019-03-26 21:15:01 -0400

    final touches

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # liri-node-app
+*first liri.js project
+
+
+# ABOUT LIRI APP:
+
+*first you need to install npm in your terminal "npm i"
+
+*call out modules, axios, moment, DotEnv
+
+   * [Node-Spotify-API](https://www.npmjs.com/package/node-spotify-api)
+
+   * [Axios](https://www.npmjs.com/package/axios)
+
+     * You'll use Axios to grab data from the [OMDB API](http://www.omdbapi.com) and the [Bands In Town API](http://www.artists.bandsintown.com/bandsintown-api)
+
+   * [Moment](https://www.npmjs.com/package/moment)
+
+   * [DotEnv](https://www.npmjs.com/package/dotenv)
+   
+# movie-this 
+* will print out the following:
+
+* Title
+* IMDB Rating
+* Country
+* Language
+* Plot
+* Actors List
+* Rotten Tomatoes Rating
+
+#spotify-this-song
+
+* will print out the following:
+
+* Artist
+* Album Name
+* Song Title
+* Preview Link
+
+
+
+# concert-this
+
+* Venue
+* Location
+* Place of event
--- liri.js
+++ liri.js
@@ -1,18 +1,23 @@
 require("dotenv").config();
 
+
+//VARIABLES
 var axios = require("axios");
-
 var keys = require("./keys.js");
-
-var go = process.argv[2];
-var value = process.argv.slice(3).join("");
-
 var Spotify = require("node-spotify-api");
+var test = new Spotify(keys.spotify);
+//format time properly
+var moment = require("moment");
+var fs = require("fs");
 
-var spotify = new Spotify(keys.spotify);
+
+var input = process.argv.slice(3).join("+");
+var go = process.argv[2];
 
 
 
+  function run() {
+  
 switch(go) {
     case "concert-this":
     concert();
@@ -27,26 +32,34 @@ switch(go) {
     break;
 
     case "do-what-it-says":
-    femaledog();
+    dowhat();
     break;
+
+    default:
+    console.log("invalid comment")
+
 }
+  }
+run()
 
 
+
+    //concert
 function concert() {
 
-    var artist = value;
+    var artist = input;
 
     axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
     .then(
         function(concertResponse){
 
-            for(var i =0; i < concert.data.length; i++) {
+            for(var i =0; i < concertResponse.data.length; i++) {
 
                 
-            var concert = conertResponse.data.venue[i]
-                console.log(concert.venue[i].name)
-               // console.log(concert.venue[i].region + " " + concert.venue[i].city)
-                //console.log(concert.venue[i].datatime)
+            var concert = concertResponse.data[i].venue
+            console.log("Concert Date: " + moment(concert.datetime).format("L"));
+            console.log("Venue Name: " + concert.name);
+            console.log("Venue City: " + concert.city); 
             }
         }
     )
@@ -62,39 +75,53 @@ function concert() {
 
 //OMDB
 
-function movie() {
+function movie(go) {
 
 
         var movieInput = "";
     
-    if(value == ""){
+    if(go == ""){
         
             movieInput = "mr.nobody"}
             else{ 
-                movieInput = value
+                movieInput = go
         }
     
     axios.get("http://www.omdbapi.com/?t=" + movieInput + "&apikey=d058edb1&")
     .then(
         function(response) {
-            let movie = response.data 
-          console.log(movie);
+         console.log("Title: " + response.data.Title);
+          console.log("Release Year: " + response.data.Year);
+          console.log("Imdb Rating: " + response.data.imdbRating);
+          console.log("Rotten Tomatoes Score: " + response.data.Ratings[0].Value);
+          console.log("Country: " + response.data.Country);
+         console.log("Language: " + response.data.Language);
+          console.log("Plot: " + response.data.Plot);
+          console.log("Actors: " + response.data.Actors);
+
+
         
         }
 )};
 
 
 
+//spotify for songs
 
-
-function songs() {
+function spotify() {
 
    
-    spotify.search({
+    test.search({
         type: 'track',
-        query: value
+        query: input,
+        limit: 1
     }).then(function(response) {
-        console.log(response);
+     console.log("Artists: " + response.tracks.items[0].album.artists[0].name);
+     console.log("Song Name: " + response.tracks.items[0].name);
+     console.log("song Preview: " + response.tracks.items[0].preview_url);
+     console.log("Album Name: " + response.tracks.items[0].album.name)
+
+        
     })
     .catch(function(err) {
         console.log(err);
@@ -103,3 +130,28 @@ function songs() {
    
 
 console.log();
+
+
+
+//create do what it says function //
+function dowhat() {
+    fs.readFile("random.txt", "utf-8", function(err, data) {
+         console.log(data);
+         if (err) 
+        console.log(err);
+         
+        var dataArr = data.split(",");
+        console.log(dataArr); 
+      
+        var dataArr2 = dataArr[1].split(" ");
+        console.log(dataArr2);
+        go = dataArr[0];
+        input = dataArr[1];
+          // console.log(input);
+          // console.log(command);
+          spotify(dataArr);
+          run();
+          
+      
+          })
+        }
